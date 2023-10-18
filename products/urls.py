1dc00a0fcc1bbb54c10d7b9ffc6b79ac9e1998dc
nikolai <nikolai.guerra25@gmail.com>
2023-10-17 20:39:02 -0500

    Seccion de productos y estilos terminada

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('detalleProducto/<slug:slug>/', views.ProductDetailView.as_view(), name="productDetail"),
     path('crearResena/<str:nombre_producto>/', views.reviewCreateView.as_view(), name="reviewCreate"),
     path('<slug:categoria_slug>/', views.ProductListView.as_view(), name="productList"),
+    path('editarProducto/<slug:slug>/', views.ProductUpdateView.as_view(), name="productUpdate"),
+    path('eliminarProducto/<slug:slug>/', views.ProductDeleteView.as_view(), name="productDelete"),
 ]
