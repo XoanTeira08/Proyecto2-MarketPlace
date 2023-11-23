b60a0bfb4bf79e9c67fd910c525777748d7520c2
Niko <nikolai.guerra25@gmail.com>
2023-11-22 20:38:13 -0500

    Shopping Cart Finished

--- products/models.py
+++ products/models.py
@@ -55,8 +55,20 @@ class Cart (models.Model):
     completed= models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.id) 
+        return str(self.id)
     
+    @property
+    def total_price(self):
+        cartitems= self.cartitems.all()
+        total= sum([item.price for item in cartitems])
+        return total
+    
+    @property
+    def num_of_items(self):
+        cartitems= self.cartitems.all()
+        quantity= sum([item.quantity for item in cartitems])
+        return quantity
+
 class CartItem (models.Model):
     product= models.ForeignKey(Product, on_delete=models.CASCADE,related_name='items')
     cart= models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='cartitems')
@@ -64,3 +76,8 @@ class CartItem (models.Model):
 
     def __str__ (self):
         return self.product.name
+    
+    @property
+    def price(self):
+        new_price= self.product.price * self.quantity
+        return new_price
