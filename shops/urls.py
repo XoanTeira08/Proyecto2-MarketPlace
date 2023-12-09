4be5dd488c5c9698782ef6eb3174b0f30e64bb03
TEIRA XOAN <xoanteira08@gmail.com>
2023-12-09 12:57:25 -0500

    Agregando Reviews a tiendas

--- shops/urls.py
+++ shops/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #path('', views.ShopListView.as_view(), name="shopList"),
     path('crearTienda/', views.ShopCreateView.as_view(), name="shopCreate"),
     path('<slug:slug>/', views.ShopDetailView.as_view(), name="shopDetail"),
+    path('crearResena/<str:nombre_tienda>/', views.reviewShopCreateView.as_view(), name="reviewShopCreate"),
     #path('actualizarTienda/<slug:slug>/', views.ShopUpdateView.as_view(), name="shopUpdate"),
     #path('eliminarTienda/<slug:slug>/', views.ShopDeleteView.as_view(), name="shopDelete"),
 ]
